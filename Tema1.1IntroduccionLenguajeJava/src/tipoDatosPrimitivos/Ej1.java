3ad45b446e463bfbe24e51a0e257b34c796c6636
aorttri2612 <aorttri2612@g.educcand.es>
2026-09-22 16:10:14 +0200

    I change the proyect to the format that Eva likes and do the first ej

---
+++ Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej1.java
@@ -0,0 +1,22 @@
+package tipoDatosPrimitivos;
+
+public class Ej1 {
+	
+	public void show () {
+		 int numero1 = 40000; //
+		 System.out.println(numero1);
+	     long numero2 = 3900750360L;
+	     System.out.println(numero2);
+	     byte numero3 = 120;
+	     System.out.println(numero3);
+	     short numero4 = 25500;
+	     System.out.println(numero4);
+
+	}
+
+	public static void main(String[] args) {
+		new Ej1().show();
+
+	}
+
+}
