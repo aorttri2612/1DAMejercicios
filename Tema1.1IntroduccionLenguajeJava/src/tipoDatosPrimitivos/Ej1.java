e2431ae1588a12010807aadf886b356df0fcb478
aorttri2612 <aorttri2612@g.educcand.es>
2026-09-22 16:20:15 +0200

    Exercise 1 and 2 in english done

--- Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej1.java
+++ Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej1.java
@@ -3,14 +3,14 @@ package tipoDatosPrimitivos;
 public class Ej1 {
 	
 	public void show () {
-		 int numero1 = 40000; //it doesn't fit in a short
-		 System.out.println(numero1);
-	     long numero2 = 3900750360L;
-	     System.out.println(numero2); // it doesn't fit in int and to indicate that it's long i have to put the l
-	     byte numero3 = 120;
-	     System.out.println(numero3); 
-	     short numero4 = 25500;
-	     System.out.println(numero4); // it doesn't fit in a byte
+		 int num1 = 40000; //it doesn't fit in a short
+		 System.out.println(num1);
+	     long num2 = 3900750360L;
+	     System.out.println(num2); // it doesn't fit in int and to indicate that it's long i have to put the l
+	     byte num3 = 120;
+	     System.out.println(num3); 
+	     short num4 = 25500;
+	     System.out.println(num4); // it doesn't fit in a byte
 
 	}
 
---
+++ Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej2.java
@@ -0,0 +1,22 @@
+package tipoDatosPrimitivos;
+
+public class Ej2 {
+	
+	public void show () {
+		
+		short s = 30000;
+		byte b;
+		
+		b = (byte) s;
+		
+		System.out.println(b); /* 48 because the byte can't take all of 
+		the s and overflows and only retains 8 bytes less significant*/
+		
+	}
+	
+	public static void main(String[] args) {
+		new Ej2().show();
+
+	}
+
+}
