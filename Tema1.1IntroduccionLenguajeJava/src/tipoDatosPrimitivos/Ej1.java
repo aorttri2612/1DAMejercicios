bda3c487b7c624af78b673b406419a0cbd4b9b8a
aorttri2612 <aorttri2612@g.educcand.es>
2026-09-22 16:12:26 +0200

    Exercise 1 with comments

--- Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej1.java
+++ Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej1.java
@@ -3,14 +3,14 @@ package tipoDatosPrimitivos;
 public class Ej1 {
 	
 	public void show () {
-		 int numero1 = 40000; //
+		 int numero1 = 40000; //it doesn't fit in a short
 		 System.out.println(numero1);
 	     long numero2 = 3900750360L;
-	     System.out.println(numero2);
+	     System.out.println(numero2); // it doesn't fit in int and to indicate that it's long i have to put the l
 	     byte numero3 = 120;
-	     System.out.println(numero3);
+	     System.out.println(numero3); 
 	     short numero4 = 25500;
-	     System.out.println(numero4);
+	     System.out.println(numero4); // it doesn't fit in a byte
 
 	}
 
