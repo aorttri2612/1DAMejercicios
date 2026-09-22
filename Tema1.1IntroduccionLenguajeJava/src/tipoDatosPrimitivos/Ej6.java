b50ad846aa7cd771465eaf5050d2be4141c1963b
aorttri2612 <aorttri2612@g.educcand.es>
2026-09-22 16:35:43 +0200

    exercise 6 done

---
+++ Tema1.1IntroduccionLenguajeJava/src/tipoDatosPrimitivos/Ej6.java
@@ -0,0 +1,21 @@
+package tipoDatosPrimitivos;
+
+public class Ej6 {
+	
+	public void show () {
+		
+		 char emoji = '❤';
+	     char decimal = 10084;
+         char hexadecimal = 0x2764;
+
+	     System.out.println(emoji);
+	     System.out.println(decimal);
+	     System.out.println(hexadecimal);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Ej6().show();
+	}
+
+}
