e3d10fa3b245abfb69d94200185837c83faf5d12
aorttri2612 <aorttri2612@g.educcand.es>
2026-09-25 10:46:26 +0200

    Exercise 1 done

--- Tema1.1IntroduccionLenguajeJava/src/expresiones/Ej1.java
+++ Tema1.1IntroduccionLenguajeJava/src/expresiones/Ej1.java
@@ -1,5 +1,16 @@
 package expresiones;
 
 public class Ej1 {
+	
+	public void show() {
+		int a = 3, b = 5, c= 4, d = 2, result, final1;
+		result = c / d ;
+		final1 = a * b - result;
+		System.out.println(final1);
+	}
 
+	public static void main(String[] args) {
+		new Ej1().show();
+
+	}
 }
